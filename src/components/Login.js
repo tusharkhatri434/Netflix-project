4f74cb2d70c8f5ef934be9404ea019d1d881a4b7
[tushar khatri] <tusharkhatri8193@gmail.com>
2023-12-09 08:11:43 +0530

    Login Form States update

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,8 +5,19 @@ import { useState } from 'react'
 const Login = () => {
   
   const [signIn,setSignIn] = useState(false);
+  const [email , setEmail] = useState("");
+  const [password , setPassword] = useState("");
+  const[name , setName] = useState("");
   function toogleForm (){
     setSignIn(!signIn);
+  }
+   
+  const formSubmitHandler = (e) =>{
+           e.preventDefault();
+           console.log(name,email,password);
+           setEmail("");
+           setName("");
+           setPassword("");
   }
 
   return (
@@ -15,10 +26,10 @@ const Login = () => {
       <img
         src={banner}
         alt="banner"
-        className="brightness-[.40]  max-sm:hidden"
+        className="brightness-[.40] w-screen min-h-screen  max-sm:hidden"
       ></img>
-      <div className=" px-12 py-16 max-sm:py-28 w-4/12 max-sm:w-full max-sm:static max-sm:rounded-none max-sm:h-[100vh]  bg-black absolute top-24 right-1/3 rounded-md bg-opacity-80">
-        <form className="flex flex-col gap-4 ">
+      <div className=" px-12 py-16 max-sm:py-28 w-[26rem] max-sm:w-full max-sm:static max-sm:rounded-none max-sm:h-[100vh]  bg-black absolute top-24 max-md:right-36 max-lg:right-1/4  right-1/3 rounded-md bg-opacity-80">
+        <form onSubmit={formSubmitHandler}  className="flex flex-col gap-4 ">
           <p className="text-4xl font-semibold mb-3 text-white">
             {signIn ? "Sign Up" : "Sign In"}
           </p>
@@ -27,19 +38,36 @@ const Login = () => {
               className="h-14 px-4 rounded-md bg-zinc-800 text-white outline-none"
               type="text"
               placeholder="Full Name"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
             />
           ) : null}
           <input
             className="h-14 px-4 rounded-md bg-zinc-800 text-white outline-none"
             type="email"
             placeholder="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
           <input
             className="h-14 rounded-md px-4 bg-zinc-800 text-white outline-none"
             type="password"
             placeholder="Password"
+            name='password'
+            autoComplete='on'
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
-          <button className="h-14 rounded-md bg-red-600 text-white mt-8">
+          <button
+            className="h-14 rounded-md bg-red-600 text-white mt-8"
+            type='submit'
+          >
             {signIn ? "Sign Up" : "Sign In"}
           </button>
           <div className="flex justify-between text-zinc-400 px-1 ">
@@ -52,7 +80,7 @@ const Login = () => {
         </form>
         <p className="mt-6 text-zinc-400">
           {signIn ? "Already have an account? " : "New to Netflix? "}
-          <button onClick={toogleForm} className="text-white cursor-pointer">
+          <button type='submit' onClick={toogleForm} className="text-white cursor-pointer">
             {signIn ? "Sign In" : "Sign Up"}
           </button>
         </p>
