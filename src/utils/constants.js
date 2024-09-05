f07d0c3364d7cc2287dc3ddc3ee09e55b0f0e5af
tusharkhatri434 <tusharkhatri8193@gmail.com>
2024-09-05 10:08:01 +0530

    added .env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,15 +1,15 @@
+const bearerToken = process.env.REACT_APP_BEARER_TOKEN;
+
 export const PROFILE_AVATAR2 = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.jpg";
 export const PROFILE_AVATAR ="https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp";
 export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTdmNzEzYzIwOGMwYTMyZTBmZmEzZGYxMWFkZjMyYyIsInN1YiI6IjY1YTAwODhiOGRlMGFlMDEyNThjMzcxMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bhVPRjh3uOjye43ig5LvQxDpjnVmb7vnkXecPBWJgL8",
+    Authorization:bearerToken
   },
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
-export const GPT_API_Key = "sk-wic8SKWiYBmyVEkK36V9T3BlbkFJS2q6B4lNs4L07l8ov9";
-
+export const GPT_API_Key = "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";
